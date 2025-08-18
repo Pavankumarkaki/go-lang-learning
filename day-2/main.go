c8715d57df6ec242b6746971ed6f0eb5428d27e5
SavithaSista <savitha.sista@apxor.com>
2025-08-18 23:56:00 +0530

    Refactor main function and update ParseCard implementation for blackjack rules

--- day-2/main.go
+++ day-2/main.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
-	var x int = 0
-	switch  {
-	case x < 0:
-		fmt.Println("Negative")
-	case x > 0:
-		fmt.Println("Positive")
+// import "fmt"
 
-	default :
-	 fmt.Println("Good Morning")
-	}
+// func main() {
+// 	var x int = 0
+// 	switch  {
+// 	case x < 0:
+// 		fmt.Println("Negative")
+// 	case x > 0:
+// 		fmt.Println("Positive")
+
+// 	default :
+// 	 fmt.Println("Good Morning")
+// 	}
+// }
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+    switch card {
+        case "ace":
+         return 11
+		case "king":
+			return 10
+    }
+	return 0
+} 
+func main() {
+	value:= ParseCard("king")
+	fmt.Println("card",value)
 }
